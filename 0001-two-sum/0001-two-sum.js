ad0d84e8a702f8f45dc9238ca315d0a28a9bc668
Brandon B <51722944+bbissing@users.noreply.github.com>
2023-07-12 23:41:31 -0500

    Time: 59 ms (90.51%), Space: 42.9 MB (40.95%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -3,16 +3,33 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-  for (var i = 0; i < nums.length; i++) {
-    for (var j = 0; j < nums.length; j++) {
-      if (j !== i) {
-        if (nums[i] + nums[j] === target) {
-          return [i, j];
-        }
-      }
+
+// O(n2) method
+// var twoSum = function(nums, target) {
+//   for (var i = 0; i < nums.length; i++) {
+//     for (var j = 0; j < nums.length; j++) {
+//       if (j !== i) {
+//         if (nums[i] + nums[j] === target) {
+//           return [i, j];
+//         }
+//       }
+//     }
+//   }
+// };
+
+// O(n) method (HashMap)
+const twoSum = function(nums, target) {
+  const obj = {};
+  for (let i = 0; i < nums.length; i++) {
+    const n = nums[i];
+    const k = target - n;
+    if (k in obj) {
+      return [obj[k], i];
     }
+    obj[n] = i;
   }
+  return false;
 };
+
 
 twoSum([2,7,11,15], 9);
