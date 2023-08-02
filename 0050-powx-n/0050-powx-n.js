3554a908c2cf719e828073bc7d719979312690a2
Brandon B <51722944+bbissing@users.noreply.github.com>
2023-08-01 22:33:58 -0500

    Time: 56 ms (78.91%), Space: 42.7 MB (11.97%) - LeetHub

--- 0050-powx-n/0050-powx-n.js
+++ 0050-powx-n/0050-powx-n.js
@@ -4,5 +4,29 @@
  * @return {number}
  */
 let myPow = function(x, n) {
-    return Math.pow(x, n);
+    function innerFunc(x, n) {
+        if (x === 0) {
+            return 0;
+        }
+        if (n === 0) {
+            return 1;
+        }
+
+        let res = innerFunc(x, Math.floor(n / 2));
+        res = res * res;
+
+        if (n % 2 === 1) {
+            return res * x;
+        }
+
+        return res;
+    }
+
+    let result = innerFunc(x, Math.abs(n));
+
+    if (n >= 0) {
+        return result;
+    }
+
+    return 1 / result;
 };
