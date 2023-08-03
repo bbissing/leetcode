04c8a6eb3838ee64a5954adc9121821b49da772f
Brandon B <51722944+bbissing@users.noreply.github.com>
2023-08-03 00:21:26 -0500

    Update 0125-valid-palindrome.js

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -4,14 +4,10 @@
  */
 var isPalindrome = function(s) {
     let temp = s.replace(/[_\W]/g, '');
-    // let temp = s.replace([^0-9a-z], '');
     temp = temp.toLowerCase();
-    console.log('temp: ', temp);
     let reverse = temp.split('').reverse().join('');
-    console.log('reverse: ', reverse);
 
     let result = temp === reverse ? true: false;
-    console.log('result: ', result);
 
     return result;
 };
