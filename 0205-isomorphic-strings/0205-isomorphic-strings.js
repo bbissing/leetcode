3b51db66a51635027b2c4d0ef02aff644b5064c1
Brandon B <51722944+bbissing@users.noreply.github.com>
2023-08-27 21:42:09 -0500

    Time: 83 ms (15.6%), Space: 49.9 MB (5.1%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.js
+++ 0205-isomorphic-strings/0205-isomorphic-strings.js
@@ -9,9 +9,6 @@ const isIsomorphic = function(s, t) {
       t: {}
     };
 
-
-    
-
     function findIndex (str, signal) {
       for (let i = 0; i < str.length; i++) {
         if (characterIndices[signal][str[i]] === undefined) {
@@ -25,23 +22,10 @@ const isIsomorphic = function(s, t) {
     findIndex(s, 's');
     findIndex(t, 't');
     
-    // console.log('characterIndices: ', characterIndices);
-    
     let tempObjOne = Object.values(characterIndices)[0];
     let tempObjTwo = Object.values(characterIndices)[1];
     let tempArrOne = Object.values(tempObjOne).sort((a, b) => a - b);
     let tempArrTwo = Object.values(tempObjTwo).sort((a, b) => a - b);
-
-    // console.log('characterIndices: ', Object.values(characterIndices)[0]);
-
-    // console.log('tempObjOne: ', tempObjOne);
-    // console.log('tempObjTwo: ', tempObjTwo);
-
-    // console.log('tempObjOne - Keys: ', Object.values(tempObjOne));
-    // console.log('tempArrOne: ', JSON.stringify(tempArrOne));
-    // console.log('tempArrTwo: ', JSON.stringify(tempArrTwo));
-
-    // console.log('boolean: ', JSON.stringify(tempArrOne) === JSON.stringify(tempArrTwo));
 
     if (JSON.stringify(tempArrOne) === JSON.stringify(tempArrTwo)) {
       return true;
